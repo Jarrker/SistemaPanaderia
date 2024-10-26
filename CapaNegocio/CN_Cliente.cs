f9119a1e8ea1db2fe293d165504fc35dc34ff4d9
Leonardo Cesías Díaz <lpcd_04@hotmail.com>
2024-10-26 11:31:48 -0500

    Test

--- CapaNegocio/CN_Cliente.cs
+++ CapaNegocio/CN_Cliente.cs
@@ -24,7 +24,7 @@ namespace CapaNegocio
 
             if (obj.DNI == "")
             {
-                Mensaje += "EEs necesario el DNI del Cliente\n";
+                Mensaje += "Es necesario el DNI del Cliente\n";
             }
 
             if (obj.NombreCompleto == "")
